1677a60e5c364f33ec13998b01009722f6bf5b7f
jeanne <harbingerhvh@gmail.com>
2024-02-14 22:27:43 -0800

    beautified with black

--- src/saturn_cli.py
+++ src/saturn_cli.py
@@ -4,8 +4,9 @@ import threading
 import simpleaudio as sa
 from pydub import AudioSegment
 import pydub.playback as playback
-class CommandLineParser:
 
+
+class CommandLineParser:
     """
     A class that parses command line arguments and executes corresponding commands.
 
@@ -37,24 +38,97 @@ class CommandLineParser:
         self.argvlen = len(argv)
         self.isPlaying = False
         # small list of audio formats
-        self.audioFormats = [".wav", ".mp3", ".ogg", ".flac", ".m4a", ".wma", ".aiff", ".alac", ".aac", ".amr", ".au", ".awb", ".dct", ".dss",
-                             ".dvf", ".gsm", ".iklax", ".ivs", ".m4p", ".mmf", ".mpc", ".msv", ".nmf", ".nsf", ".oga", ".mogg", ".opus", ".ra",
-                             ".rm", ".raw", ".sln", ".tta", ".vox", ".wv", ".webm", ".8svx"]
+        self.audioFormats = [
+            ".wav",
+            ".mp3",
+            ".ogg",
+            ".flac",
+            ".m4a",
+            ".wma",
+            ".aiff",
+            ".alac",
+            ".aac",
+            ".amr",
+            ".au",
+            ".awb",
+            ".dct",
+            ".dss",
+            ".dvf",
+            ".gsm",
+            ".iklax",
+            ".ivs",
+            ".m4p",
+            ".mmf",
+            ".mpc",
+            ".msv",
+            ".nmf",
+            ".nsf",
+            ".oga",
+            ".mogg",
+            ".opus",
+            ".ra",
+            ".rm",
+            ".raw",
+            ".sln",
+            ".tta",
+            ".vox",
+            ".wv",
+            ".webm",
+            ".8svx",
+        ]
 
     def print_help(self):
         print("usage:", "python", self.argv[0], "--help")
         # print all commands and what they do
         print("Commands\t\tDescription\t\tUsage")
-        print("-h,--help\t\tprint this help message.\t\tpython " + self.argv[0] + " --help")
-        print("-c,--count\t\tcount the number of arguments.\t\tpython " + self.argv[0] + " --count")
-        print("-p,--play\t\tplay a file.\t\tpython " + self.argv[0] + " --play file_path")
-        print("-s,--sequential\t\tplay files sequentially.\t\tpython " + self.argv[0] + " --sequential file_path1 file_path2 ...")
-        print("-o,--overlap\t\tplay files overlapping each other.\t\tpython " + self.argv[0] + " --overlap file_path1 file_path2 ...")
-        print("-l,--list\t\tlist all audio files in the current directory recursively.\t\tpython " + self.argv[0] + " --list")
-        print("-r,--rename\t\trename an audio file.\t\tpython " + self.argv[0] + " --rename original_name new_name")
-        print("-t,--transcode\t\tchange audio format.\t\tpython " + self.argv[0] + " --transcode original_name new_name file_extension")
-        print("-b,--play-backwards\t\tplay a file backwards.\t\tpython " + self.argv[0] + " --play-backwards file_path")
-        print("-a,--concatenate\t\tconcatenate audio files.\t\tpython " + self.argv[0] + " --concatenate file_path1 file_path2 ... extension")
+        print(
+            "-h,--help\t\tprint this help message.\t\tpython "
+            + self.argv[0]
+            + " --help"
+        )
+        print(
+            "-c,--count\t\tcount the number of arguments.\t\tpython "
+            + self.argv[0]
+            + " --count"
+        )
+        print(
+            "-p,--play\t\tplay a file.\t\tpython " + self.argv[0] + " --play file_path"
+        )
+        print(
+            "-s,--sequential\t\tplay files sequentially.\t\tpython "
+            + self.argv[0]
+            + " --sequential file_path1 file_path2 ..."
+        )
+        print(
+            "-o,--overlap\t\tplay files overlapping each other.\t\tpython "
+            + self.argv[0]
+            + " --overlap file_path1 file_path2 ..."
+        )
+        print(
+            "-l,--list\t\tlist all audio files in the current directory recursively.\t\tpython "
+            + self.argv[0]
+            + " --list"
+        )
+        print(
+            "-r,--rename\t\trename an audio file.\t\tpython "
+            + self.argv[0]
+            + " --rename original_name new_name"
+        )
+        print(
+            "-t,--transcode\t\tchange audio format.\t\tpython "
+            + self.argv[0]
+            + " --transcode original_name new_name file_extension"
+        )
+        print(
+            "-b,--play-backwards\t\tplay a file backwards.\t\tpython "
+            + self.argv[0]
+            + " --play-backwards file_path"
+        )
+        print(
+            "-a,--concatenate\t\tconcatenate audio files.\t\tpython "
+            + self.argv[0]
+            + " --concatenate file_path1 file_path2 ... extension"
+        )
         sys.exit(0)
 
     def count_arguments(self):
@@ -76,7 +150,14 @@ class CommandLineParser:
             self.isPlaying = False
         else:
             self.isPlaying = True
-            sound = AudioSegment.from_file(file_path, format=file_path.split(".")[-1] if file_path[0] != "." else file_path[1:].split(".")[-1])
+            sound = AudioSegment.from_file(
+                file_path,
+                format=(
+                    file_path.split(".")[-1]
+                    if file_path[0] != "."
+                    else file_path[1:].split(".")[-1]
+                ),
+            )
             playback.play(sound)
             self.isPlaying = False
 
@@ -99,7 +180,6 @@ class CommandLineParser:
                 print("Playing:", file_path)
                 self.play(file_path)
                 queue = queue[1:]
-
 
     def play_command(self):
         # play a file using the play method
@@ -147,24 +227,39 @@ class CommandLineParser:
             sys.exit(1)
 
     def list_command(self):
-        #print all files in the current directory recursively with audio file extensions
+        # print all files in the current directory recursively with audio file extensions
         for root, dirs, files in os.walk(os.getcwd()):
             for file in files:
-                if file[-4:] in self.audioFormats or file[-5:] in self.audioFormats or file[-6:] in self.audioFormats or file[-3:] in self.audioFormats:
+                if (
+                    file[-4:] in self.audioFormats
+                    or file[-5:] in self.audioFormats
+                    or file[-6:] in self.audioFormats
+                    or file[-3:] in self.audioFormats
+                ):
                     print(os.path.join(root, file))
 
     def rename_command(self):
         # rename an audio file, take the original name and the new one, doesn't change file extension
         if self.argvlen > 3:
             original_name = self.argv[2]
-            extension = original_name.split(".")[-1] if original_name[0] != "." else original_name[1:].split(".")[-1]
+            extension = (
+                original_name.split(".")[-1]
+                if original_name[0] != "."
+                else original_name[1:].split(".")[-1]
+            )
             new_name = self.argv[3]
             if "." not in new_name[1:] or "." not in original_name[1:]:
                 print("Error: Please provide the file extension(s).", file=sys.stderr)
                 sys.exit(1)
             elif original_name[1:].split(".")[-1] != new_name[1:].split(".")[-1]:
-                print('This function does not convert between audio formats. Using original file extension...')
-            new_name = new_name.split(".")[0] if new_name[0] != "." else "." + new_name[1:].split(".")[0] + "." + extension
+                print(
+                    "This function does not convert between audio formats. Using original file extension..."
+                )
+            new_name = (
+                new_name.split(".")[0]
+                if new_name[0] != "."
+                else "." + new_name[1:].split(".")[0] + "." + extension
+            )
             os.rename(original_name, new_name)
         else:
             print(
@@ -172,14 +267,26 @@ class CommandLineParser:
                 file=sys.stderr,
             )
             sys.exit(1)
-    
+
     def transcode_command(self):
         # change audio format
         # usage python saturn_cli.py -t original_name new_name file_extension
         if self.argvlen > 4:
-            original_name = self.argv[2].split(".")[0] if self.argv[2][0] != "." else "." + self.argv[2][1:].split(".")[0]
-            new_name = self.argv[3].split(".")[0] if self.argv[3][0] != "." else "." + self.argv[3][1:].split(".")[0]
-            extension = self.argv[-1].split(".")[-1] if self.argv[4][0] != "." else self.argv[4][1:].split(".")[-1]
+            original_name = (
+                self.argv[2].split(".")[0]
+                if self.argv[2][0] != "."
+                else "." + self.argv[2][1:].split(".")[0]
+            )
+            new_name = (
+                self.argv[3].split(".")[0]
+                if self.argv[3][0] != "."
+                else "." + self.argv[3][1:].split(".")[0]
+            )
+            extension = (
+                self.argv[-1].split(".")[-1]
+                if self.argv[4][0] != "."
+                else self.argv[4][1:].split(".")[-1]
+            )
             sound = AudioSegment.from_file(original_name, format=extension)
             sound.export(new_name + "." + extension, format=extension)
         else:
@@ -188,7 +295,6 @@ class CommandLineParser:
                 file=sys.stderr,
             )
             sys.exit(1)
-
 
     def play_backwards_command(self):
         # play a file using the play method
@@ -205,17 +311,26 @@ class CommandLineParser:
             )
             sys.exit(1)
 
-
     def concatenate_command(self):
         # concatenate multiple audio files (>2) into one file
         # take the file paths, the new name, the new file extension, and the crossfade amount
         if self.argvlen > 4:
             file_paths = self.argv[2:-1]
-            new_name = self.argv[-2].split(".")[0] if self.argv[-2][0] != "." else "." + self.argv[-2][1:].split(".")[0]
-            extension = self.argv[-1].split(".")[-1] if self.argv[-1][0] != "." else self.argv[-1][1:].split(".")[-1]
+            new_name = (
+                self.argv[-2].split(".")[0]
+                if self.argv[-2][0] != "."
+                else "." + self.argv[-2][1:].split(".")[0]
+            )
+            extension = (
+                self.argv[-1].split(".")[-1]
+                if self.argv[-1][0] != "."
+                else self.argv[-1][1:].split(".")[-1]
+            )
             sound = AudioSegment.from_file(file_paths[0], format=extension)
             for file_path in file_paths[1:]:
-                sound = sound.append(AudioSegment.from_file(file_path, format=extension), crossfade=1000)
+                sound = sound.append(
+                    AudioSegment.from_file(file_path, format=extension), crossfade=1000
+                )
             sound.export(new_name + "." + extension, format=extension)
         else:
             print(
@@ -259,6 +374,7 @@ class CommandLineParser:
                 )
                 print("Try", self.argv[0], "--help")
                 sys.exit(1)
+
 
 if __name__ == "__main__":
     # create a command line parser and parse the command line arguments
