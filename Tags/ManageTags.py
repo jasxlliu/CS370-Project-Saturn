3109d586f75089d1ad5a1e4ba0e400dbccdb0a3a
Chris G <gomezc@whitman.edu>
2024-02-27 14:47:10 -0800

    created tags folder

---
+++ Tags/ManageTags.py
@@ -0,0 +1,15 @@
+class ManageTags:
+    def __init__(self, tags):
+        self.tags = []
+    
+    def create_tag():
+        pass
+    
+    def favorite_tag():
+        pass
+    
+    # takes in tag name to remove.
+    def remove_tag():
+        pass
+
+    
