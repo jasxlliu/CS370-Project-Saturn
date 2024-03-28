7ff53c4e8cff9b2f9b7cc26a82f69d2cb203c6e2
Chris G <gomezc@whitman.edu>
2024-03-27 23:34:23 -0700

    added docs

--- Database/Playlist.py
+++ Database/Playlist.py
@@ -1,25 +1,43 @@
 class Playlist:
     """
-    On a mySQL workbench, we have defined a sounds database which holds info on how we are storing 
+    Represents a playlist management system for storing sounds in a MySQL database.
+
+    Attributes:
+        None (No specific attributes defined in this class)
+
+    Methods:
+        __init__(self):
+            Initializes the Playlist object. Creates a connection to the MySQL database.
+            TODO: Initialize table SoundList to hold info on the default loaded sounds
+                  from our GitHub sounds file (refer to UML diagram).
+
+        create_database_connection(self):
+            Establishes a secure connection to the MySQL Workbench.
+            TODO: Implement the connection logic.
+
+        create(self):
+            Adds an option for the user to create a new playlist.
+            TODO: Show updates related to playlist creation in the MySQL Workbench.
+
+        add_sound(self):
+            Allows the user to add a sound to an existing playlist.
+            TODO: Implement the logic for adding sounds to the playlist.
+
+        remove_sound(self):
+            Enables the user to remove a sound from an existing playlist.
+            TODO: Implement the logic for removing sounds from the playlist.
     """
     def __init__(self):
-        """
-        TODO: Initialize table SoundList to hold info on the default loaded sounds we have in our github sounds file (refer to UML diagram).  
-        """
         pass
     
     def create_database_connection(self):
-        """TODO: secure connection to mysql workbench."""
         pass
     
     def create(self):
-        """TODO: Add an option for user to create a playlist + show updates on mysql workbench."""
         pass
     
     def add_sound(self):
-        """if user wants to add a sound to an existing playlist."""
         pass
     
     def remove_sound(self):
-        """if user wants to remove a sound from an existing playlist."""
         pass
