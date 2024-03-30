a8b1fb6b046d6a6e77d0fb52f55539f55a3a3a11
Chris G <gomezc@whitman.edu>
2024-03-30 00:00:58 -0700

    Finished setting updatabase!!

--- Database/SQL.sql
+++ Database/SQL.sql
@@ -1,20 +1,14 @@
-CREATE TABLE SoundPlaylistsInfo (
-	SoundTitle VARCHAR(50) NOT NULL,
-    PlaylistTitle VARCHAR (25),
-    PRIMARY KEY(SoundTitle, PlaylistTitle)
-);
-
 CREATE TABLE SoundList (
 	Title VARCHAR(50) NOT NULL UNIQUE,
     Length FLOAT NOT NULL,
     IsEdited BOOLEAN NOT NULL,
     DateCreated DATETIME NOT NULL,
-    PRIMARY KEY(Title),
-    FOREIGN KEY(Title) REFERENCES SoundPlaylistsInfo(SoundTitle)
+    PRIMARY KEY(Title)
 );
 
--- example of user creating a playlist called "liked"
-CREATE TABLE Liked (
-	SoundTitle VARCHAR(50) NOT NULL UNIQUE,
-    PRIMARY KEY(SoundTitle)
+CREATE TABLE SoundPlaylistsInfo (
+	SoundTitle VARCHAR(50) NOT NULL,
+    PlaylistTitle VARCHAR (25),
+    PRIMARY KEY(SoundTitle, PlaylistTitle),
+    FOREIGN KEY(SoundTitle) REFERENCES SoundList(Title)
 );
